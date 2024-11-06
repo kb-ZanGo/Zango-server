df4ce50896d858cc4aca90b11cbd3cba048a52ea
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-11-06 10:19:54 +0900

    feat: first commit

---
+++ src/main/java/kb/zango/ZangoApplication.java
@@ -0,0 +1,13 @@
+package kb.zango;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZangoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZangoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kb/zango/ZangoApplicationTests.java
@@ -0,0 +1,13 @@
+package kb.zango;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZangoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
