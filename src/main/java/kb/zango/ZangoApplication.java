488d78ff24d133e0b4aa7478d9283967690096ba
hyunjilee1132 <hyunji.lee1132@gmail.com>
2024-11-16 00:16:06 +0900

    feat: board 에러났는데 일단 올려봄..

--- src/main/java/kb/zango/ZangoApplication.java
+++ src/main/java/kb/zango/ZangoApplication.java
@@ -1,11 +1,13 @@
 package kb.zango;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@MapperScan("kb.zango.domain.diary.mapper")
 public class ZangoApplication {
 
     public static void main(String[] args) {
--- src/main/java/kb/zango/domain/diary/mapper/BoardMapper.java
+++ src/main/java/kb/zango/domain/diary/mapper/BoardMapper.java
@@ -1,19 +1,19 @@
 package kb.zango.domain.diary.mapper;
 
+import java.util.List;
 import kb.zango.domain.diary.board.entity.Board;
 import org.apache.ibatis.annotations.Mapper;
-import java.util.List;
 
-//@Mapper
+@Mapper
 public interface BoardMapper {
 
     void insertBoard(Board board);
 
-//    Board findBoardById(Long boardId);
-//
-//    List<Board> findAllBoards();
-//
-//    void updateBoard(Board board);
-//
-//    void deleteBoard(Long boardId);
+    Board findBoardById(Long boardId);
+
+    List<Board> findAllBoards();
+
+    void updateBoard(Board board);
+
+    void deleteBoard(Long boardId);
 }
