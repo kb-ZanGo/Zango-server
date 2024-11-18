0ab3fbf7e90cb092690294895b96ec93c91f5074
wjdgus9808 <dbswjdgus9808@gmail.com>
2024-11-18 11:45:37 +0900

    feat: 거래내역추가

--- src/main/java/kb/zango/domain/transaction/TransactionService.java
+++ src/main/java/kb/zango/domain/transaction/TransactionService.java
@@ -1,6 +1,8 @@
 package kb.zango.domain.transaction;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import io.codef.api.EasyCodef;
 import io.codef.api.EasyCodefServiceType;
 import io.codef.api.EasyCodefUtil;
@@ -32,7 +34,7 @@ public class TransactionService {
     public void publishToken() throws IOException {
 
     }
-
+    @PostConstruct
     public String register() throws IOException, InterruptedException {
         codef.setClientInfoForDemo(clientId, clientSecret);
         codef.setPublicKey(publicKey);
@@ -59,10 +61,43 @@ public class TransactionService {
         parameterMap.put("accountList", accountList);
 
 /** #6.계정 등록 요청(Connected ID 발급 요청)	- 서비스타입(API:정식, DEMO:데모, SANDBOX:샌드박스) */
-        String result = codef.createAccount(EasyCodefServiceType.DEMO, parameterMap);
+//        String connectedIdResponse = codef.createAccount(EasyCodefServiceType.DEMO, parameterMap);
+//        String connectedId = extractConnectedIdFromResponse(connectedIdResponse);
 
 /** #7.결과 확인	*/
+//        System.out.println(connectedIdResponse);
+
+        /** #5.요청 파라미터 설정 - 각 상품별 파라미터를 설정(https://developer.codef.io/products) */
+        HashMap<String, Object> param = new HashMap<String, Object>();
+//        param.put("connectedId", connectedId);
+        param.put("connectedId", "7TcJJamz4Wjb4.6So1JePo");
+        param.put("organization", "0004");
+        param.put("account", "55660201830185");
+        param.put("startDate", "20240901");
+        param.put("endDate", "20241117");
+        param.put("orderBy", "0");
+
+/** #6.코드에프 정보 조회 요청 - 서비스타입(API:정식, DEMO:데모, SANDBOX:샌드박스) */
+        String productUrl = "/v1/kr/bank/p/account/transaction-list";	// (예시)개인 보유카드 조회 URL
+        String result = codef.requestProduct(productUrl, EasyCodefServiceType.DEMO, param);
+
+
+
+/**	#7.코드에프 정보 결과 확인	*/
         System.out.println(result);
+
         return result;
+    }
+
+    private String extractConnectedIdFromResponse(String response) throws IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        ObjectNode rootNode = (ObjectNode) mapper.readTree(response);
+
+        // "data" 객체 안에 있는 "connectedId" 필드 추출
+        if (rootNode.has("data") && rootNode.get("data").has("connectedId")) {
+            return rootNode.get("data").get("connectedId").asText();
+        } else {
+            throw new IOException("connectedId not found in response");
+        }
     }
 }
